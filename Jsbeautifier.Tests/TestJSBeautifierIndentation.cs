391083b774df717fb9b63d7a25c537fa06a314b5
Denis Ivanov <ghost181991@gmail.com>
2018-02-23 01:19:26 +0300

    MS Test replaced with NUnit

---
+++ Jsbeautifier.Tests/TestJSBeautifierIndentation.cs
@@ -0,0 +1,33 @@
+﻿using Jsbeautifier;
+using NUnit.Framework;
+
+namespace Tests
+{
+    [TestFixture]
+    public class TestJSBeautifierIndentation
+    {
+        private Beautifier beautifier;
+
+        public TestJSBeautifierIndentation()
+        {
+            beautifier = new Beautifier();
+            BeautifierOptions bo = beautifier.Opts;
+            bo.IndentWithTabs = true;
+        }
+
+        [Test]
+        public void TestTabs()
+        {
+            Assert.AreEqual(beautifier.Beautify("{tabs()}"), "{\n\ttabs()\n}");
+        }
+
+        [Test]
+        public void TestFunctionIndent()
+        {
+            BeautifierOptions bo = beautifier.Opts;
+            bo.IndentWithTabs = true;
+            bo.KeepArrayIndentation = true;
+            Assert.AreEqual(beautifier.Beautify("var foo = function(){ bar() }();"), "var foo = function() {\n\tbar()\n}();");
+        }
+    }
+}
